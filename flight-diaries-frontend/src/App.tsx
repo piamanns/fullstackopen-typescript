7d03c10cccf9059f1d9324b655873423e27ea97a
Pia Manns <pia.manns@gmail.com>
2023-09-02 23:33:14 +0300

    Add basic functionality for adding new diary entries from flight diary front-end (exercise 9.17)

--- flight-diaries-frontend/src/App.tsx
+++ flight-diaries-frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import axios from 'axios';
 import { useState, useEffect } from "react";
-import { DiaryEntry } from "./types";
-
-const baseUrl = 'http://localhost:3001/api/diaries';
+import { DiaryEntry, NewDiaryEntry, AddEntryFunction } from "./types";
+import { getAllEntries, addNewEntry } from './services/diaryService';
 
 const Entry = ({ entry }: { entry: DiaryEntry }) => {
   return (
@@ -17,18 +15,82 @@ const Entry = ({ entry }: { entry: DiaryEntry }) => {
   );
 };
 
+const EntryForm = ( { createEntry }: {createEntry: AddEntryFunction}) => {
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newComment, setNewComment] = useState('');
+
+  const submitDiaryEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+     createEntry({
+      date: newDate,
+      visibility: newVisibility,
+      weather: newWeather,
+      comment: newComment
+    });
+
+    setNewDate('');
+    setNewWeather('');
+    setNewVisibility('');
+    setNewComment('');
+  };
+
+  return (
+    <form onSubmit={submitDiaryEntry}>
+      <div>
+        date:{" "}
+        <input
+          value={newDate}
+          onChange={(event) => setNewDate(event.target.value)}
+        />
+      </div>
+      <div>
+        weather:{" "}
+        <input
+          value={newWeather}
+          onChange={(event) => setNewWeather(event.target.value)}
+        />
+      </div>
+      <div>
+        visibility:{" "}
+        <input
+          value={newVisibility}
+          onChange={(event) => setNewVisibility(event.target.value)}
+        />
+      </div>
+      <div>
+        comment:{" "}
+        <input
+          value={newComment}
+          onChange={(event) => setNewComment(event.target.value)}
+        />
+      </div>
+      <button type='submit'>add</button>
+    </form>
+  );
+};
+
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
 
   useEffect(() => {
-    axios.get(baseUrl).then(response => {
-      setEntries(response.data);
+    getAllEntries().then(response => {
+      setEntries(response);
     });
   }, []);
 
+  const addDiaryEntry = (entry: NewDiaryEntry) => {
+    addNewEntry(entry).then(data => {
+      setEntries(entries.concat(data));
+    });
+  };
+
   return (
     <div>
-      <h3>Diary Entries:</h3>
+      <h3>Add new entry</h3>
+      <EntryForm createEntry={addDiaryEntry} />
+      <h3>Diary Entries</h3>
         {entries.map(entry =>
           <Entry key={entry.id} entry={entry} />
         )}
