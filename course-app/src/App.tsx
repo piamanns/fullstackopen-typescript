0b5cd40f582310568e0cdbf0e20f0dfaf8b7e842
Pia Manns <pia.manns@gmail.com>
2023-09-02 14:35:27 +0300

    Add exercise 9.15 (extending app from 9.14)

--- course-app/src/App.tsx
+++ course-app/src/App.tsx
@@ -1,51 +1,143 @@
-interface CourseData {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
 }
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: "special"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 
 const Header = ({ name }: {name: string}) => {
   return (
     <div>
       <h1>{name}</h1>
     </div>
-  )
+  );
 };
 
-const Content = ({ courseParts }: {courseParts: CourseData[]}) => {
+const Content = ({ courseParts }: {courseParts: CoursePart[]}) => {
+  const getCourseData = (part: CoursePart) => {
+    switch (part.kind) {
+      case "basic":
+        return (
+          <p>
+            <strong>{part.name} {part.exerciseCount}</strong><br />
+            <em>{part.description}</em>
+          </p>
+        );
+      case "group":
+        return (
+          <p>
+            <strong>{part.name} {part.exerciseCount}</strong><br />
+            project exercises: {part.groupProjectCount}
+          </p>
+        );
+      case "background":
+        return (
+          <p>
+            <strong>{part.name} {part.exerciseCount}</strong><br />
+            <em>{part.description}</em><br />
+            {part.backgroundMaterial}
+          </p>
+        );
+      case "special":
+        return (
+          <p>
+            <strong>{part.name} {part.exerciseCount}</strong><br />
+            <em>{part.description}</em><br />
+            required skills: {part.requirements.join(", ")}
+          </p>
+        );
+      default:
+        return assertNever(part);
+    }
+  };
+
   return (
-    <>
-      {courseParts.map(part =>
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
-      )}
-    </>
-  )
+    <div>
+      {courseParts.map(part => {
+        return getCourseData(part);
+      })}
+    </div>
+  );
 };
 
-const Total = ({ courseParts }: {courseParts: CourseData[]}) => {
+const Total = ({ courseParts }: {courseParts: CoursePart[]}) => {
   return (
     <p>
       Number of exercises{" "}
       {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
     </p>
-  )
+  );
 };
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
